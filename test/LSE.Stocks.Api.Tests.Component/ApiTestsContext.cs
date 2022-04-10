32e418b796a25564c3cb29784e8545170fe456b2
Mark Gossa <16326726+markgossa@users.noreply.github.com>
2022-04-10 12:06:45 +0100

    Add tests class fixture

---
+++ test/LSE.Stocks.Api.Tests.Component/ApiTestsContext.cs
@@ -0,0 +1,33 @@
+﻿using LSE.Stocks.Application.Repositories;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System.Net.Http;
+
+namespace LSE.Stocks.Api.Tests.Component
+{
+    public class ApiTestsContext : IDisposable
+    {
+        public Mock<IShareExchangeRepository> MockShareExchangeRepository { get; } = new();
+        public HttpClient HttpCleint { get; }
+        private readonly WebApplicationFactory<Startup> _webApplicationFactory;
+
+        public ApiTestsContext()
+        {
+            _webApplicationFactory = BuildWebApplicationFactory();
+            HttpCleint = _webApplicationFactory.CreateClient();
+        }
+
+        private WebApplicationFactory<Startup> BuildWebApplicationFactory()
+            => new WebApplicationFactory<Startup>()
+            .WithWebHostBuilder(b => b.ConfigureServices(services
+                => ((ServiceCollection)services).AddSingleton(MockShareExchangeRepository.Object)));
+
+        public void Dispose()
+        {
+            HttpCleint.Dispose();
+            _webApplicationFactory.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
--- test/LSE.Stocks.Api.Tests.Component/SaveShareExchangeTests.cs
+++ test/LSE.Stocks.Api.Tests.Component/SaveShareExchangeTests.cs
@@ -1,8 +1,5 @@
 ﻿using LSE.Stocks.Api.Models;
-using LSE.Stocks.Application.Repositories;
 using LSE.Stocks.Domain.Models.Shares;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using System.Net;
 using System.Net.Http;
@@ -13,9 +10,12 @@ using Xunit;
 
 namespace LSE.Stocks.Api.Tests.Component;
 
-public class SaveShareExchangeTests
+public class SaveShareExchangeTests : IClassFixture<ApiTestsContext>
 {
     private const string _shareExchangeApiRoute = "shareexchange";
+    private readonly ApiTestsContext _context;
+
+    public SaveShareExchangeTests(ApiTestsContext context) => _context = context;
 
     [Theory]
     [InlineData("NASDAQ:AAPL", 10, 1, "BR10834")]
@@ -23,83 +23,66 @@ public class SaveShareExchangeTests
     public async Task GivenValidShareExchangeRequest_WhenPostEndpointCalled_ThenSavesShareExchangeAndReturnsOK(
         string tickerSymbol, decimal price, decimal count, string brokerId)
     {
-        var mockShareExchangeRepository = new Mock<IShareExchangeRepository>();
-
         var shareExchangeRequest = new SaveShareExchangeRequest(tickerSymbol, price, count, brokerId);
-        var response = await PostShareExchangeAsync(mockShareExchangeRepository.Object, shareExchangeRequest);
+        var response = await PostShareExchangeAsync(shareExchangeRequest);
 
-        AssertShareExchangeSaved(mockShareExchangeRepository, shareExchangeRequest);
+        AssertShareExchangeSaved(shareExchangeRequest);
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
-    
+
     [Theory]
     [InlineData("012345678901234567891", 10, 1, "BR10834")]
     public async Task GivenValidShareExchangeRequestWithTickerSymbolOver20Chars_WhenPostEndpointCalled_ThenDoesNotShareExchangeAndReturnsBadRequest(
         string tickerSymbol, decimal price, decimal count, string brokerId)
     {
-        var mockShareExchangeRepository = new Mock<IShareExchangeRepository>();
-
         var shareExchangeRequest = new SaveShareExchangeRequest(tickerSymbol, price, count, brokerId);
-        var response = await PostShareExchangeAsync(mockShareExchangeRepository.Object, shareExchangeRequest);
+        var response = await PostShareExchangeAsync(shareExchangeRequest);
 
-        AssertShareExchangeNotSaved(mockShareExchangeRepository, shareExchangeRequest);
+        AssertShareExchangeNotSaved(shareExchangeRequest);
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
-    
+
     [Theory]
     [InlineData("NASDAQ:AAPL", 0, 1, "BR10834")]
     [InlineData("NASDAQ:AAPL", -1, 1, "BR10834")]
     public async Task GivenValidShareExchangeRequestWithPriceZeroOrLess_WhenPostEndpointCalled_ThenDoesNotShareExchangeAndReturnsBadRequest(
         string tickerSymbol, decimal price, decimal count, string brokerId)
     {
-        var mockShareExchangeRepository = new Mock<IShareExchangeRepository>();
-
         var shareExchangeRequest = new SaveShareExchangeRequest(tickerSymbol, price, count, brokerId);
-        var response = await PostShareExchangeAsync(mockShareExchangeRepository.Object, shareExchangeRequest);
+        var response = await PostShareExchangeAsync(shareExchangeRequest);
 
-        AssertShareExchangeNotSaved(mockShareExchangeRepository, shareExchangeRequest);
+        AssertShareExchangeNotSaved(shareExchangeRequest);
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
-    
+
     [Theory]
     [InlineData("NASDAQ:AAPL", 150, 0, "BR10834")]
     [InlineData("NASDAQ:AAPL", 200, -1, "BR10834")]
     public async Task GivenValidShareExchangeRequestWithCountZeroOrLess_WhenPostEndpointCalled_ThenDoesNotShareExchangeAndReturnsBadRequest(
         string tickerSymbol, decimal price, decimal count, string brokerId)
     {
-        var mockShareExchangeRepository = new Mock<IShareExchangeRepository>();
-
         var shareExchangeRequest = new SaveShareExchangeRequest(tickerSymbol, price, count, brokerId);
-        var response = await PostShareExchangeAsync(mockShareExchangeRepository.Object, shareExchangeRequest);
+        var response = await PostShareExchangeAsync(shareExchangeRequest);
 
-        AssertShareExchangeNotSaved(mockShareExchangeRepository, shareExchangeRequest);
+        AssertShareExchangeNotSaved(shareExchangeRequest);
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 
-    private static async Task<HttpResponseMessage> PostShareExchangeAsync(IShareExchangeRepository shareExchangeRepository,
-        SaveShareExchangeRequest shareExchangeRequest) 
-            => await BuildClient(shareExchangeRepository).PostAsync(_shareExchangeApiRoute, BuildHttpContent(shareExchangeRequest));
-
-    private static HttpClient BuildClient(IShareExchangeRepository shareExchangeRepository) 
-        => new WebApplicationFactory<Startup>()
-            .WithWebHostBuilder(b => b.ConfigureServices(services
-                => ((ServiceCollection)services).AddSingleton(shareExchangeRepository)))
-                    .CreateClient();
+    private async Task<HttpResponseMessage> PostShareExchangeAsync(SaveShareExchangeRequest shareExchangeRequest)
+            => await _context.HttpCleint.PostAsync(_shareExchangeApiRoute, BuildHttpContent(shareExchangeRequest));
 
     private static StringContent BuildHttpContent(SaveShareExchangeRequest shareExchangeRequest)
         => new(JsonSerializer.Serialize(shareExchangeRequest), Encoding.UTF8, MediaTypeNames.Application.Json);
-    
-    private static void AssertShareExchangeSaved(Mock<IShareExchangeRepository> mockShareExchangeRepository, 
-        SaveShareExchangeRequest shareExchangeRequest)
-            => mockShareExchangeRepository.Verify(m => m.SaveShareExchangeAsync(MapToShareExchange(shareExchangeRequest)),
+
+    private void AssertShareExchangeSaved(SaveShareExchangeRequest shareExchangeRequest)
+            => _context.MockShareExchangeRepository.Verify(m => m.SaveShareExchangeAsync(MapToShareExchange(shareExchangeRequest)),
                 Times.Once);
 
     private static ShareExchange MapToShareExchange(SaveShareExchangeRequest saveShareExchangeRequest)
         => new(saveShareExchangeRequest.TickerSymbol, saveShareExchangeRequest.Price,
             saveShareExchangeRequest.Count, saveShareExchangeRequest.BrokerId);
 
-    private static void AssertShareExchangeNotSaved(Mock<IShareExchangeRepository> mockShareExchangeRepository, 
-        SaveShareExchangeRequest shareExchangeRequest)
-            => mockShareExchangeRepository.Verify(m => m.SaveShareExchangeAsync(MapToShareExchange(shareExchangeRequest)),
+    private void AssertShareExchangeNotSaved(SaveShareExchangeRequest shareExchangeRequest)
+            => _context.MockShareExchangeRepository.Verify(m => m.SaveShareExchangeAsync(MapToShareExchange(shareExchangeRequest)),
                 Times.Never);
 }
