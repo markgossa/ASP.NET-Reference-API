3349a82d242a5cefd371dba09b8ed5913a566f40
Mark Gossa <16326726+markgossa@users.noreply.github.com>
2022-05-02 16:37:11 +0100

    Adds API versioning and updates tests

--- src/LSE.Stocks.Api/Startup.cs
+++ src/LSE.Stocks.Api/Startup.cs
@@ -1,10 +1,14 @@
 ﻿using LSE.Stocks.Api.Middleware;
 using LSE.Stocks.Api.ServiceCollectionExtensions;
+using LSE.Stocks.Api.Swagger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System.IO;
 using System.Reflection;
@@ -17,20 +21,33 @@ public class Startup
 
     public Startup(IConfiguration configuration) => Configuration = configuration;
 
-    public void ConfigureServices(IServiceCollection services) 
-        => services.AddEndpointsApiExplorer()
-            .AddSwaggerGen(o => AddSwaggerDocumentation(o))
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer()
+            .AddSwaggerGen(o => ConfigureSwaggerGenOptions(o))
             .AddMediatorServices()
             .AddApplicationInsightsTelemetry()
             .AddRepositories(Configuration)
+            .AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerGenOptions>()
             .AddControllers();
-    
-    public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+
+        AddApiVersioning(services);
+    }
+
+    public static void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+        IApiVersionDescriptionProvider apiVersionDescriptionProvider)
     {
         if (env.IsDevelopment())
         {
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(o =>
+            {
+                foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+                {
+                    o.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                        $"LSE.Stocks.API - {description.GroupName.ToUpper()}");
+                }
+            });
         }
 
         app.UseHttpsRedirection();
@@ -41,9 +58,30 @@ public class Startup
         app.UseEndpoints(endpoints => endpoints.MapControllers());
     }
 
-    private static void AddSwaggerDocumentation(SwaggerGenOptions o)
+    private static void ConfigureSwaggerGenOptions(SwaggerGenOptions o)
+    {
+        AddSwaggerXmlComments(o);
+        o.OperationFilter<SwaggerDefaultValues>();
+    }
+
+    private static void AddSwaggerXmlComments(SwaggerGenOptions o)
     {
         var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
         o.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    }
+
+    private static void AddApiVersioning(IServiceCollection services)
+    {
+        services.AddApiVersioning(o =>
+        {
+            o.AssumeDefaultVersionWhenUnspecified = true;
+            o.DefaultApiVersion = new ApiVersion(1, 0);
+        });
+
+        services.AddVersionedApiExplorer(o =>
+        {
+            o.GroupNameFormat = "'v'VVV";
+            o.SubstituteApiVersionInUrl = true;
+        });
     }
 }
---
+++ test/LSE.Stocks.Api.Tests.Component/V1/GetSharePriceV1Tests.cs
@@ -0,0 +1,89 @@
+﻿using LSE.Stocks.Api.Models;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using Xunit;
+
+namespace LSE.Stocks.Api.Tests.Component.V1;
+
+public class GetSharePriceV1Tests : IClassFixture<ApiTestsContext>
+{
+    private const string _apiRoute = "shareprice";
+    private const string _apiRouteV1 = "v1/shareprice";
+    private readonly ApiTestsContext _context;
+
+    public GetSharePriceV1Tests(ApiTestsContext context) => _context = context;
+
+    [Theory]
+    [InlineData("NASDAQ:AAPL", 16.67, _apiRoute)]
+    [InlineData("NASDAQ:TSLA", 250, _apiRouteV1)]
+    public async Task GivenValidSharePriceRequest_WhenGetEndpointCalled_ThenReturnsAveragePriceAndReturnsOK(
+        string tickerSymbol, decimal expectedPrice, string apiRoute)
+    {
+        var response = await GetSharePriceAsync(tickerSymbol, apiRoute);
+        var price = await DeserializeResponseAsync(response);
+
+        Assert.Equal(expectedPrice, price?.Price);
+        Assert.Equal(tickerSymbol, price?.TickerSymbol);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Theory]
+    [InlineData("NOTFOUND", _apiRoute)]
+    [InlineData("NOTFOUND", _apiRouteV1)]
+    public async Task GivenSharePriceRequestForInvalidShare_WhenGetEndpointCalled_ThenReturnsNotFound(
+        string tickerSymbol, string apiRoute)
+    {
+        var response = await GetSharePriceAsync(tickerSymbol, apiRoute);
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Theory]
+    [InlineData("012345678901234567891", _apiRoute)]
+    [InlineData("012345678901234567891", _apiRouteV1)]
+    public async Task GivenSharePriceRequestForTickerSymbolOver20Chars_WhenGetEndpointCalled_ThenReturnsBadRequest(
+        string tickerSymbol, string apiRoute)
+    {
+        var response = await GetSharePriceAsync(tickerSymbol, apiRoute);
+
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    [Theory]
+    [InlineData("", _apiRoute)]
+    [InlineData(" ", _apiRoute)]
+    [InlineData(null, _apiRoute)]
+    [InlineData("", _apiRouteV1)]
+    [InlineData(" ", _apiRouteV1)]
+    [InlineData(null, _apiRouteV1)]
+    public async Task GivenSharePriceRequestForTickerSymbolEmpty_WhenGetEndpointCalled_ThenReturnsBadRequest(
+        string tickerSymbol, string apiRoute)
+    {
+        var response = await GetSharePriceAsync(tickerSymbol, apiRoute);
+
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    [Theory]
+    [InlineData("NASDAQ:ERROR", _apiRoute)]
+    [InlineData("NASDAQ:ERROR", _apiRouteV1)]
+    public async Task GivenSharePriceReositoryHasError_WhenGetEndpointCalled_ThenReturnsInternalServerError(
+        string tickerSymbol, string apiRoute)
+    {
+        var response = await GetSharePriceAsync(tickerSymbol, apiRoute);
+
+        Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+    }
+
+    private static async Task<SharePriceResponse?> DeserializeResponseAsync(HttpResponseMessage response)
+    {
+        var json = await response.Content.ReadAsStringAsync();
+
+        return JsonSerializer.Deserialize<SharePriceResponse>(json,
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+    }
+
+    private async Task<HttpResponseMessage> GetSharePriceAsync(string tickerSymbol, string apiRoute)
+            => await _context.HttpClient.GetAsync($"{apiRoute}?tickerSymbol={tickerSymbol}");
+}
