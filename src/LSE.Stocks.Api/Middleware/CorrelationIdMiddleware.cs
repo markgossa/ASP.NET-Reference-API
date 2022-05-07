1e6e954d9da306950c93b4733bf85de24fb4904f
Mark Gossa <16326726+markgossa@users.noreply.github.com>
2022-05-07 08:31:28 +0100

    Code cleanup

--- src/LSE.Stocks.Api/Middleware/CorrelationIdMiddleware.cs
+++ src/LSE.Stocks.Api/Middleware/CorrelationIdMiddleware.cs
@@ -1,23 +1,22 @@
 ﻿using LSE.Stocks.Api.Services;
 using Microsoft.AspNetCore.Http;
 
-namespace LSE.Stocks.Api.Middleware
+namespace LSE.Stocks.Api.Middleware;
+
+public class CorrelationIdMiddleware
 {
-    public class CorrelationIdMiddleware
+    private readonly RequestDelegate _next;
+
+    public CorrelationIdMiddleware(RequestDelegate next) => _next = next;
+
+    public async Task Invoke(HttpContext context, ICorrelationIdService correlationIdService)
     {
-        private readonly RequestDelegate _next;
-
-        public CorrelationIdMiddleware(RequestDelegate next) => _next = next;
-
-        public async Task Invoke(HttpContext context, ICorrelationIdService correlationIdService)
+        context.Response.OnStarting(() =>
         {
-            context.Response.OnStarting(() =>
-            {
-                context.Response.Headers.Add("X-Correlation-Id", new[] { correlationIdService.CorrelationId });
-                return Task.CompletedTask;
-            });
+            context.Response.Headers.Add("X-Correlation-Id", new[] { correlationIdService.CorrelationId });
+            return Task.CompletedTask;
+        });
 
-            await _next(context);
-        }
+        await _next(context);
     }
 }
